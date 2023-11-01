80ebdfdd50e778237e47ddf0cbf95fd488bad378
YALIM GÜRBÜZ <yalim.gurbuz@std.yildiz.edu.tr>
2023-11-01 22:39:32 +0300

    dbcontext

---
+++ EntityFrameworkCodeFirst/Contexts/NorthwindContext.cs
@@ -0,0 +1,17 @@
+﻿using EntityFrameworkCodeFirst.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkCodeFirst.Contexts
+{
+    public class NorthwindContext: DbContext    
+    {
+       
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
--- EntityFrameworkCodeFirst/Program.cs
+++ EntityFrameworkCodeFirst/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+using EntityFrameworkCodeFirst.Contexts;
+
+namespace EntityFrameworkCodeFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new NorthwindContext())
+            {
+                var customers = context.Customers.ToList();
+                foreach (var customer in customers)
+                {
+                    Console.WriteLine($"{customer.CustomerId} {customer.ContactName}");
+                }
+            }
+        }
+    }
+}
