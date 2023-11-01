38f08360145d6684c3aec7506f112169259c08ca
YALIM GÜRBÜZ <yalim.gurbuz@std.yildiz.edu.tr>
2023-11-01 22:31:51 +0300

    entites

--- EntityFrameworkCodeFirst/Entities/Customer.cs
+++ EntityFrameworkCodeFirst/Entities/Customer.cs
@@ -2,6 +2,10 @@
 {
     public class Customer
     {
+        public Customer()
+        {
+            Orders = new List<Order>();
+        }
         public string CustomerId { get; set; }
         public string ContactName { get; set; }
         public string CompanyName { get; set; }
