5d49bb810c88bda57abaa33aece5340cd82af0d5
YALIM GÜRBÜZ <yalim.gurbuz@std.yildiz.edu.tr>
2023-11-01 23:04:36 +0300

    group method

--- EntityFrameworkCodeFirst/Program.cs
+++ EntityFrameworkCodeFirst/Program.cs
@@ -9,6 +9,45 @@ namespace EntityFrameworkCodeFirst
         {
             using (var context = new NorthwindContext())
             {
+                //RemoveOrder();
+                //UpdateOrder();
+                //FindCustomer();
+                //AddCustomer();
+            }
+        }
+
+        private static void Group()
+        {
+            using (var context = new NorthwindContext())
+            {
+                var result = from c in context.Customers
+                             group c by c.Country into g
+                             select g;
+                foreach (var group in result)
+                {
+                    Console.WriteLine(group.Key);
+                    foreach (var customer in group)
+                    {
+                        Console.WriteLine($"  {customer.CustomerId} {customer.ContactName}");
+                    }
+                }
+            }
+        }
+
+        private static void Update()
+        {
+            using (var context = new NorthwindContext())
+            {
+                List<Customer> result = (from c in context.Customers
+                                         where c.Country == "Germany"
+                                         select c).ToList();
+            }
+        }
+
+        private static void RemoveOrder()
+        {
+            using (var context = new NorthwindContext())
+            {
                 Order order = context.Orders.Find(1);
                 Console.WriteLine($"{order.OrderId} {order.CustomerId} {order.Customer.ContactName}");
                 context.Orders.Remove(order);
@@ -16,7 +55,7 @@ namespace EntityFrameworkCodeFirst
             }
         }
 
-        private static void RemoveOrder()
+        private static void UpdateOrder()
         {
             using (var context = new NorthwindContext())
             {
