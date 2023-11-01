ea067ec827e4e1b8eaeb420bcc3db9348e4767ec
YALIM GÜRBÜZ <yalim.gurbuz@std.yildiz.edu.tr>
2023-11-01 22:48:29 +0300

    methods

--- EntityFrameworkCodeFirst/Program.cs
+++ EntityFrameworkCodeFirst/Program.cs
@@ -2,12 +2,48 @@
 
 
 using EntityFrameworkCodeFirst.Contexts;
+using EntityFrameworkCodeFirst.Entities;
+using System.Runtime.CompilerServices;
 
 namespace EntityFrameworkCodeFirst
 {
     class Program
     {
         static void Main(string[] args)
+        {
+
+            using (var context = new NorthwindContext())
+            {
+               Customer customer = context.Customers.Find("ALFKI");
+               customer.Orders.Add(new Order
+               {
+                   OrderDate = DateTime.Now,
+                   RequiredDate = DateTime.Now.AddDays(7),
+                   ShipCity = "Berlin",
+                   ShipCountry = "Germany"
+               });
+                context.SaveChanges();
+
+            }
+
+         
+        }
+        private static void AddCustomer()
+        {
+            using (var context = new NorthwindContext())
+            {
+                var customer = new Customer
+                {
+                    CustomerId = "ALFKI",
+                    ContactName = "Maria Anders",
+                    CompanyName = "Alfreds Futterkiste",
+                    Country = "Germany"
+                };
+                context.Customers.Add(customer);
+                context.SaveChanges();
+            }
+        }
+        private static void ConnectDbSet()
         {
             using (var context = new NorthwindContext())
             {
