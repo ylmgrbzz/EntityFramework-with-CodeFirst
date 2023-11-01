3f128c515c723267f5a0845cdca543c245f069d5
YALIM GÜRBÜZ <yalim.gurbuz@std.yildiz.edu.tr>
2023-11-01 22:29:42 +0300

    Entities

---
+++ EntityFrameworkCodeFirst/Entities/Customer.cs
@@ -0,0 +1,12 @@
+﻿namespace EntityFrameworkCodeFirst.Entities
+{
+    public class Customer
+    {
+        public string CustomerId { get; set; }
+        public string ContactName { get; set; }
+        public string CompanyName { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public List<Order> Orders { get; set; }
+    }
+}
---
+++ EntityFrameworkCodeFirst/Entities/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkCodeFirst.Entities
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public string CustomerId { get; set; } 
+        public DateTime OrderDate { get; set; }
+        public DateTime RequiredDate { get; set; }
+        public string ShipCity { get; set; }
+        public string ShipCountry { get; set; }
+        public Customer Customer { get; set; }
+    }
+}
